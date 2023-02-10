c7e0f2606011b10d890a60d2a0b7be4bd3c66def
knuksg <knuksg@naver.com>
2023-02-10 15:25:51 +0900

    test

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser, PermissionsMixin
 
 # Create your models here.
 class User(AbstractUser, PermissionsMixin):
-    username = models.CharField(max_length=16, unique=False, blank=True, null=True, default='None')
+    username = models.CharField(max_length=16,)
     email = models.CharField(max_length=100, unique=True)
     mbti = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
