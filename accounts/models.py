ef313f35872da86e3cb7bbee4d0e2e80dfb6e85f
knuksg <knuksg@naver.com>
2023-02-15 11:56:58 +0900

    fix

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser, PermissionsMixin):
+class User(AbstractUser):
     username = models.CharField(max_length=16, unique=True)
     email = models.CharField(max_length=100, unique=True)
     mbti = models.CharField(max_length=100, blank=True, null=True)
