2331c3dd1226ad1609dfa33f5868e99847e5ca7d
knuksg <knuksg@naver.com>
2023-02-27 12:52:44 +0900

    fix

--- accounts/models.py
+++ accounts/models.py
@@ -20,6 +20,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=30)
     mbti = models.CharField(max_length=100, blank=True, null=True)
+    height = models.CharField(max_length=30)
+    weight = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_staff = models.BooleanField(default=False)
