4a7bd8d2422ec9f2549a95e45cdfa643d1d289fd
knuksg <knuksg@naver.com>
2023-02-27 13:15:48 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -124,9 +124,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
