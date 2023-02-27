484c67f9078ebbafc2bb20a2a63f40115201c507
knuksg <knuksg@naver.com>
2023-02-27 13:23:12 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lz9p*y#zc8xf_csp=3x(ys#dr2)4dwazu#qp6+uuj&$x9fd-)k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
