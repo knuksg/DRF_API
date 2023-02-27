cb74f0b9116f387662a913ebe4a8dbbc9a0460d4
knuksg <knuksg@naver.com>
2023-02-27 12:50:10 +0900

    fix

--- calendars/admin.py
+++ calendars/admin.py
@@ -3,7 +3,7 @@ from .models import CalendarEvent
 
 @admin.register(CalendarEvent)
 class CalendarEventAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'start', 'end', 'user']
+    list_display = ['id', 'user','title', 'start', 'end', 'isAllDay']
     list_filter = ['user']
     search_fields = ['title', 'user__email']
     # date_hierarchy = 'start'
