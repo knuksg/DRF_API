290396c57e7df2166af75a8993c1b66e33b8670d
knuksg <knuksg@naver.com>
2023-03-02 11:10:16 +0900

    save

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import User
+from .serializers import UserSerializer
 
-# Register your models here.
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'gender', 'birthday', 'height', 'weight', 'mbti')
+    search_fields = ('username', 'email',)
+    list_filter = ('gender', 'mbti',)
+    ordering = ('-id',)
+
+    def get_serializer(self, request):
+        return UserSerializer
+
+admin.site.register(User, UserAdmin)
