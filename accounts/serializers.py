9c7274f7efc0df0b29324a0262ecedf4905b68fa
knuksg <knuksg@naver.com>
2023-03-02 10:58:01 +0900

    save

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'mbti', 'height', 'weight']
+        fields = ['username', 'email', 'gender', 'birthday', 'height', 'weight', 'mbti']
