779707d0cf552e113268bcbe8e0a6e13e55860cc
knuksg <knuksg@naver.com>
2023-03-22 17:37:51 +0900

    save

--- chatgpt/admin.py
+++ chatgpt/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from .models import Conversation
 
-class MessageInline(admin.StackedInline):
-    model = Conversation
-    extra = 0
-
 class ConversationAdmin(admin.ModelAdmin):
-    inlines = [MessageInline]
     list_display = ('user',)
-    search_fields = ['user__username', 'Conversation__conversation']
+    search_fields = ['user__username',]
 
 admin.site.register(Conversation, ConversationAdmin)
